3692cefbef530fd088978f5a4fc30d565e6ab67a
unknown <ankitbkana@outlook.com>
2023-11-15 19:39:37 +0530

    auto select country based on location

--- js/script.js
+++ js/script.js
@@ -5,64 +5,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const stateSelect = document.getElementById('stateSelect');
     const citySelect = document.getElementById('citySelect');
 
-  // Replace this URL with the correct path to your JSON file containing countries, states, and cities
-  
-  // Fetch countries from the JSON
-  fetch('js/countries.json')
-      .then((response) => response.json())
-      .then((data) => {
-        data.countries.forEach((country) => {
-          if(ourcountry.includes(country.name)){
-              // console.log(country.name)
-              const option = document.createElement('option');
-              option.value = country.name;
-              option.textContent = country.name;
-              option.setAttribute("dataValue", country.id)
-              option.setAttribute("sortname", country.sortname)
-              // option.dataValue = country.id;
-              countrySelect.appendChild(option);
-            }
-          });
-      })
-      .catch((error) => console.error('Error:', error));
+    // Replace this URL with the correct path to your JSON file containing countries, states, and cities
 
-var url = 'http://www.geoplugin.net/json.gp?jsoncallback=?';
-$.getJSON(url)
-            .success(function(data){
-                var country_code = data.geoplugin_countryCode;
-                var $country = $('#countrySelect');
-                $country.find('option[sortname="'+country_code+'"]').prependTo($country);
-                $country.find('option[sortname=""]').text('--------------');
-                $country.val(country_code);
+    // Fetch countries from the JSON
+    fetch('js/countries.json')
+        .then((response) => response.json())
+        .then((data) => {
+            data.countries.forEach((country) => {
+                if (ourcountry.includes(country.name)) {
+                    // console.log(country.name)
+                    const option = document.createElement('option');
+                    option.value = country.name;
+                    option.textContent = country.name;
+                    option.setAttribute("dataValue", country.id)
+                    option.setAttribute("sortname", country.sortname)
+                    // option.dataValue = country.id;
+                    countrySelect.appendChild(option);
+                }
             });
-  // On country selection change, load respective states
-  countrySelect.addEventListener('change', function () {
-      stateSelect.disabled = false;
-      citySelect.disabled = true;
-      stateSelect.innerHTML = '<option value="">-- Select State --</option>';
-      var selectedCountry = countrySelect.options[countrySelect.selectedIndex].getAttribute('dataValue');
-      // console.log(selectedCountry);
-      // console.log("hii");
-      // Fetch states based on the selected country
-      fetch('js/states.json')
-          .then((response) => response.json())
-          .then((data1) => {
-              data1.states.forEach(state => {
-                  if (state.country_id == selectedCountry) {
-                      // console.log(state)
-                      const option = document.createElement('option');
-                      option.value = state.name;
-                      option.setAttribute("dataValue", state.id)
-                      option.textContent = state.name;
-                      stateSelect.appendChild(option);
-                  }
-              });
+        })
+        .catch((error) => console.error('Error:', error));
+
+    var url = 'http://www.geoplugin.net/json.gp?jsoncallback=?';
+    $.getJSON(url)
+        .success(function (data) {
+            var country_code = data.geoplugin_countryCode;
+            var $country = $('#countrySelect');
+            $country.find('option[sortname="' + country_code + '"]').prependTo($country);
+            $country.find('option[sortname=""]').text('--------------');
+            $country.val(country_code);
+
 
             var selectedCountry = countrySelect.options[countrySelect.selectedIndex].getAttribute('dataValue');
-            // console.log(selectedCountry);
-
-
-            // console.log("hii");
+            console.log(selectedCountry);
 
             // Fetch states based on the selected country
             fetch('js/states.json')
@@ -84,33 +59,34 @@ $.getJSON(url)
                 .catch((error) => console.error('Error:', error));
             //   });
         });
+});
 
-    // On state selection change, load respective cities
-    stateSelect.addEventListener('change', function () {
-        citySelect.disabled = false;
-        citySelect.innerHTML = '<option value="">-- Select City --</option>';
+// On state selection change, load respective cities
+stateSelect.addEventListener('change', function () {
+    citySelect.disabled = false;
+    citySelect.innerHTML = '<option value="">-- Select City --</option>';
 
-        var selectedState = stateSelect.options[stateSelect.selectedIndex].getAttribute('dataValue');
-        // console.log(selectedState);
-        // Fetch cities based on the selected state and country
-        fetch('js/cities.json')
-            .then((response) => response.json())
-            .then((data3) => {
-                // console.log(data3)
-                data3.cities.forEach((city) => {
-                    if (city.state_id == selectedState) {
-                        const option = document.createElement('option');
-                        option.value = city.name;
-                        option.textContent = city.name;
-                        citySelect.appendChild(option);
-                    }
-                });
+    var selectedState = stateSelect.options[stateSelect.selectedIndex].getAttribute('dataValue');
+    // console.log(selectedState);
+    // Fetch cities based on the selected state and country
+    fetch('js/cities.json')
+        .then((response) => response.json())
+        .then((data3) => {
+            // console.log(data3)
+            data3.cities.forEach((city) => {
+                if (city.state_id == selectedState) {
+                    const option = document.createElement('option');
+                    option.value = city.name;
+                    option.textContent = city.name;
+                    citySelect.appendChild(option);
+                }
+            });
 
-            })
-            .catch((error) => console.error('Error:', error));
-    });
+        })
+        .catch((error) => console.error('Error:', error));
 });
 
 
 
-})
+
+
