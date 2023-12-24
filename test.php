d410d43d40dfb1479f99184e34ed22684a051a64
unknown <ankitbkana@outlook.com>
2023-12-24 20:30:06 +0530

    df

--- test.php
+++ test.php
@@ -7,18 +7,35 @@
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+    body {
+      background: #f7e092;
+    }
+
+    .picbg {
+      background: rgb(34, 193, 195);
+      background: linear-gradient(0deg, rgba(34, 193, 195, 0) 52%, rgba(255, 0, 0, 1) 53%);
+    }
+  </style>
 </head>
+
 
 <body>
   <div class="container">
-    <div class="row">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="row mt-3">
+      <div class="card p-0 overflow-hidden" style="width: 18rem;">
+        <div class="d-flex picbg">
+          <div style="width:30%" class="pic relative m-2">
+            <img class="absolute rounded-circle bg-white border-white" width="70px" src="imgs\defaultusergh.png" alt="Profile">
+          </div>
+          <div class="name">
+            <h2 class="card-title">Ankit</h2>
+            <div class="phone d-flex gap-3 text-danger"> 8888888888 <a href=""><i class="fa-solid fa-phone text-success fs-4"></i> </a><a href="#"><i class="fa-solid fs-3 fa-brands fa-whatsapp text-success "></i></a></div>
+          </div>
+        </div>
+        <div class="d-flex flex-column">
+          buttom
         </div>
       </div>
     </div>
