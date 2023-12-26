3b86e9c7ddbfd8e28918af698a63f29c8ab95fdc
unknown <ankitbkana@outlook.com>
2023-12-26 21:19:10 +0530

    df

--- js/music.js
+++ js/music.js
@@ -10,111 +10,106 @@ var next_track = document.querySelector(".next-track");
 var prev_track = document.querySelector(".prev-track");
 
 function playpauseTrack() {
-  if (currentAudio.paused) {
-    currentAudio.play();
-    playPause.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
-  } else {
-    currentAudio.pause();
-    playPause.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-  }
+    if (currentAudio.paused) {
+        currentAudio.play();
+        playPause.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+    } else {
+        currentAudio.pause();
+        playPause.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+    }
 }
 
 Array.from(Allmusic).forEach((element) => {
-  element.addEventListener("click", (e) => {
-    currentAudio.src = e.target.dataset.src;
-    playTrackName.innerHTML = e.target.innerHTML;
-    playPause.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-    console.log(e.target);
-    let a = document.createElement("audio");
-    a.src = e.target.dataset.src;
-    console.log(a);
-    const duration = a.duration;
-    console.log(duration);
-    const totalMinutes = Math.floor(duration / 60);
-    const totalSeconds = Math.floor(duration % 60);
-    totalTimeDisplay.textContent = `${totalMinutes}:${
-      totalSeconds < 10 ? "0" : ""
-    }${totalSeconds}`;
-  });
+    element.addEventListener("click", (e) => {
+        currentAudio.src = e.target.dataset.src;
+        playTrackName.innerHTML = e.target.innerHTML;
+        playPause.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+        console.log(e.target);
+        let a = document.createElement("audio");
+        a.src = e.target.dataset.src;
+        console.log(a);
+        const duration = a.duration;
+        console.log(duration);
+        const totalMinutes = Math.floor(duration / 60);
+        const totalSeconds = Math.floor(duration % 60);
+        totalTimeDisplay.textContent = `${totalMinutes}:${totalSeconds < 10 ? "0" : ""
+            }${totalSeconds}`;
+    });
 });
 currentAudio.addEventListener("timeupdate", () => {
-  const currentTime = currentAudio.currentTime;
-  const duration = currentAudio.duration;
+    const currentTime = currentAudio.currentTime;
+    const duration = currentAudio.duration;
 
-  const currentMinutes = Math.floor(currentTime / 60);
-  const currentSeconds = Math.floor(currentTime % 60);
-  const totalMinutes = Math.floor(duration / 60);
-  const totalSeconds = Math.floor(duration % 60);
+    const currentMinutes = Math.floor(currentTime / 60);
+    const currentSeconds = Math.floor(currentTime % 60);
+    const totalMinutes = Math.floor(duration / 60);
+    const totalSeconds = Math.floor(duration % 60);
 
-  currentTimeDisplay.textContent = `${currentMinutes}:${
-    currentSeconds < 10 ? "0" : ""
-  }${currentSeconds}`;
-  totalTimeDisplay.textContent = `${totalMinutes}:${
-    totalSeconds < 10 ? "0" : ""
-  }${totalSeconds}`;
+    currentTimeDisplay.textContent = `${currentMinutes}:${currentSeconds < 10 ? "0" : ""
+        }${currentSeconds}`;
+    totalTimeDisplay.textContent = `${totalMinutes}:${totalSeconds < 10 ? "0" : ""
+        }${totalSeconds}`;
 
-  // const progress = (currentTime / duration) * 100;
+    // const progress = (currentTime / duration) * 100;
 
-  seekPosition = currentAudio.currentTime * (100 / currentAudio.duration);
-  seek_slider.value = seekPosition;
+    seekPosition = currentAudio.currentTime * (100 / currentAudio.duration);
+    seek_slider.value = seekPosition;
 });
 function setVolume() {
-  // Set the volume according to the
-  // percentage of the volume slider set
-  currentAudio.volume = volume_slider.value / 100;
+    // Set the volume according to the
+    // percentage of the volume slider set
+    currentAudio.volume = volume_slider.value / 100;
 }
 function seekTo() {
-  // Calculate the seek position by the
-  // percentage of the seek slider
-  // and get the relative duration to the track
-  let seekto = currentAudio.duration * (seek_slider.value / 100);
+    // Calculate the seek position by the
+    // percentage of the seek slider
+    // and get the relative duration to the track
+    let seekto = currentAudio.duration * (seek_slider.value / 100);
 
-  // Set the current track position to the calculated seek position
-  currentAudio.currentTime = seekto;
+    // Set the current track position to the calculated seek position
+    currentAudio.currentTime = seekto;
 }
-// function nextTrack() {
-//   let music = Number(currentAudio.dataset.music) + 1;
-//   let a = document.getElementById(music);
-//   console.log(a)
-//   currentAudio.src = a.dataset.src;
-//   playTrackName.innerHTML = a.innerHTML;
-//   currentAudio.dataset.music = music
-// }
-function prevTrack() {
-  let music = Number(currentAudio.dataset.music);
-  console.log(music)
-  if (music= 0) {
-    let a = document.getElementById(music);
-    console.log(a);
-    currentAudio.src = a.dataset.src;
-    playTrackName.innerHTML = a.innerHTML;
-    currentAudio.dataset.music = music;
-  } else {
-    console.log("No next track available");
+
+if(currentAudio.dataset.music == '0'){
     prev_track.style.opacity = "0.3";
-  }
+}
+function prevTrack() {
+    next_track.style.opacity = "1";
+    let music = Number(currentAudio.dataset.music);
+    
+    if (music = 0) {
+        let a = document.getElementById(music);
+        
+        currentAudio.src = a.dataset.src;
+        playTrackName.innerHTML = a.innerHTML;
+        currentAudio.dataset.music = music;
+    } else {
+        console.log("No next track available");
+        prev_track.style.opacity = "0.3";
+    }
 }
 
 function nextTrack() {
-  let totalMusicCount = document.querySelectorAll(".music-item");
-  totalMusicCount = Array.from(totalMusicCount).length - 1;
-  console.log(totalMusicCount);
-  let music = Number(currentAudio.dataset.music) + 1;
+    prev_track.style.opacity = "1";
+    let totalMusicCount = document.querySelectorAll(".music-item");
+    totalMusicCount = Array.from(totalMusicCount).length - 1;
+    console.log(totalMusicCount);
+    let music = Number(currentAudio.dataset.music) + 1;
 
-  // Check if the next music is within the total count
-  if (music <= totalMusicCount) {
-    let a = document.getElementById(music);
-    //   console.log(a);
-    currentAudio.src = a.dataset.src;
-    playTrackName.innerHTML = a.innerHTML;
-    currentAudio.dataset.music = music;
-  } else {
-    // If there is no next music, you can add a message or perform any other action
-    console.log("No next track available");
-    next_track.style.opacity = "0.3";
-    // Optionally, you can stop playback or loop back to the first track
-    // currentAudio.src = document.getElementById(1).dataset.src;
-    // playTrackName.innerHTML = document.getElementById(1).innerHTML;
-    // currentAudio.dataset.music = 1;
-  }
+    // Check if the next music is within the total count
+    if (music <= totalMusicCount) {
+        let a = document.getElementById(music);
+        //   console.log(a);
+        currentAudio.src = a.dataset.src;
+        playTrackName.innerHTML = a.innerHTML;
+        currentAudio.dataset.music = music;
+    } else {
+        // If there is no next music, you can add a message or perform any other action
+        console.log("No next track available");
+        next_track.style.opacity = "0.3";
+        // Optionally, you can stop playback or loop back to the first track
+        // currentAudio.src = document.getElementById(1).dataset.src;
+        // playTrackName.innerHTML = document.getElementById(1).innerHTML;
+        // currentAudio.dataset.music = 1;
+    }
 }
--- test.php
+++ test.php
@@ -7,15 +7,44 @@
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <style>
     body {
       background: #f7e092;
     }
 
+    * {
+      box-sizing: border-box;
+    }
+
     .picbg {
       background: rgb(34, 193, 195);
-      background: linear-gradient(0deg, rgba(34, 193, 195, 0) 52%, rgba(255, 0, 0, 0.2) 53%);
+      background: linear-gradient(0deg, rgba(34, 193, 195, 0) 65%, rgba(255, 0, 0, 0.1) 35%);
+    }
+
+    .cardbox {
+      transition: all 1s;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 500px));
+      gap: 20px;
+      /* grid-template-columns: repeat( auto-fill, minmax(300px, auto) ); */
+      grid-template-columns: repeat(auto-fill, minmax(max-content, auto));
+      justify-content: center;
+      /* grid-template-columns: repeat(auto-fill, 40px);   */
+    }
+
+    .card {
+      width: 100%;
+      transition: all 1s;
+    }
+    .table td, .table{
+      padding: 0;
+      margin: 0;
+    }
+    .table{
+      width: 100%;
     }
   </style>
 </head>
@@ -23,22 +52,135 @@
 
 <body>
   <div class="container">
-    <div class="row mt-3">
-      <div class="card p-0 overflow-hidden" style="width: 18rem;">
+    <div class="row mt-3 cardbox">
+      <!-- <div class="profile">
+
+      </div> -->
+      <div class="card p-0 overflow-hidden">
         <div class="d-flex picbg">
-          <div style="width:32%" class="pic relative m-2">
-            <img style="border:2px solid white" class="absolute rounded-circle bg-white border-white" width="90px" src="imgs\defaultusergh.png" alt="Profile">
+          <div style="width:25%" class="pic relative m-2">
+            <img style="border:2px solid white" class="absolute rounded-circle bg-white border-white" width="100%"
+              src="imgs\defaultusergh.png" alt="Profile">
+            <p class="d-flex gap-2 flex-items-center justify-content-center px-2 m-0">
+              <a href=""><i class="fa-solid fa-phone text-success fs-4"></i> </a>
+              <a href="#"><i class="fa-solid fs-4  fa-brands fa-whatsapp text-success "></i></a>
+              <a href="#"><i class="fa-solid fs-4   fa-envelope text-success "></i></a>
+            </p>
           </div>
-          <div class="name d-flex flex-column justify-content-center">
-            <h2 class="card-title py-2">Ankit</h2>
-            <div class="phone d-flex gap-2 text-danger align-items-center"> 8888888888 <a href=""><i class="fa-solid fa-phone rounded-circle shadow p-2 text-success fs-5"></i> </a><a href="#"><i class="fa-solid fs-5 rounded-circle shadow p-2 fa-brands fa-whatsapp text-success "></i></a></div>
+          <div style="width:67%" class="name d-flex flex-column justify-content-around">
+            <h2 class="card-title m-0 p-0">Ankit</h2>
+            <div class="phone d-flex flex-column-reverse text-danger  align-items-around">
+              <table  class="table">
+                <tbody>
+                  <tr>
+                    <td>Samuh:</td>
+                    <td>GIEO Gita</td>
+                  </tr>
+                  <tr>
+                    <td>Phone:</td>
+                    <td>8888888888</td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td>ankitbkana@outlook.com</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- <p class="m-0">8888888888</p>
+              <p class="m-0">ankitbkana@outlook.com</p> -->
+            </div>
           </div>
         </div>
-        <hr class="m-2 mx-3 ">
-        <div class="d-flex flex-column p-3">
-          buttom
+        <hr class="m-0 mx-3 ">
+        <div class="d-flex gap-2 p-3">
+          <div class="btn btn-danger">Edit</div>
+          <div class="btn btn-danger">Delete</div>
         </div>
       </div>
+      <div class="card p-0 overflow-hidden">
+        <div class="d-flex picbg">
+          <div style="width:25%" class="pic relative m-2">
+            <img style="border:2px solid white" class="absolute rounded-circle bg-white border-white" width="100%"
+              src="imgs\defaultusergh.png" alt="Profile">
+            <p class="d-flex gap-2 flex-items-center justify-content-center px-2 m-0">
+              <a href=""><i class="fa-solid fa-phone text-success fs-4"></i> </a>
+              <a href="#"><i class="fa-solid fs-4  fa-brands fa-whatsapp text-success "></i></a>
+              <a href="#"><i class="fa-solid fs-4   fa-envelope text-success "></i></a>
+            </p>
+          </div>
+          <div style="width:67%" class="name d-flex flex-column justify-content-around">
+            <h2 class="card-title m-0 p-0">Ankit</h2>
+            <div class="phone d-flex flex-column-reverse text-danger  align-items-around">
+              <table  class="table">
+                <tbody>
+                  <tr>
+                    <td>Samuh:</td>
+                    <td>GIEO Gita</td>
+                  </tr>
+                  <tr>
+                    <td>Phone:</td>
+                    <td>8888888888</td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td>ankitbkana@outlook.com</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- <p class="m-0">8888888888</p>
+              <p class="m-0">ankitbkana@outlook.com</p> -->
+            </div>
+          </div>
+        </div>
+        <hr class="m-0 mx-3 ">
+        <div class="d-flex gap-2 p-3">
+          <div class="btn btn-danger">Edit</div>
+          <div class="btn btn-danger">Delete</div>
+        </div>
+      </div>
+
+      <div class="card p-0 overflow-hidden">
+        <div class="d-flex picbg">
+          <div style="width:25%" class="pic relative m-2">
+            <img style="border:2px solid white" class="absolute rounded-circle bg-white border-white" width="100%"
+              src="imgs\defaultusergh.png" alt="Profile">
+            <p class="d-flex gap-2 flex-items-center justify-content-center px-2 m-0">
+              <a href=""><i class="fa-solid fa-phone text-success fs-4"></i> </a>
+              <a href="#"><i class="fa-solid fs-4  fa-brands fa-whatsapp text-success "></i></a>
+              <a href="#"><i class="fa-solid fs-4   fa-envelope text-success "></i></a>
+            </p>
+          </div>
+          <div style="width:67%" class="name d-flex flex-column justify-content-around">
+            <h2 class="card-title m-0 p-0">Ankit</h2>
+            <div class="phone d-flex flex-column-reverse text-danger  align-items-around">
+              <table  class="table text-muted">
+                <tbody>
+                  <tr>
+                    <td>Samuh:</td>
+                    <td>GIEO Gita</td>
+                  </tr>
+                  <tr>
+                    <td>Phone:</td>
+                    <td>8888888888</td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td>ankitbkana@outlook.com</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- <p class="m-0">8888888888</p>
+              <p class="m-0">ankitbkana@outlook.com</p> -->
+            </div>
+          </div>
+        </div>
+        <hr class="m-0 mx-3 ">
+        <div class="d-flex gap-2 p-3">
+          <div class="btn btn-danger">Edit</div>
+          <div class="btn btn-danger">Delete</div>
+        </div>
+      </div>
+      
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
