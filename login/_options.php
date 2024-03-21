83b8ef86544bace106db8b83afb275a5ed9e2b48
unknown <ankitbkana@outlook.com>
2024-03-22 00:30:39 +0530

    new custem message page added

--- login/_options.php
+++ login/_options.php
@@ -1,7 +1,7 @@
 <div class="container bg-danger-subtle my-2 mb-3 shadow rounded-3 sticky-top">
     <div class="  py-2 my-2 rounded d-flex gap-2 flex-wrap justify-content-between">
-        <img data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" 
-            style="width: 40px; cursor:pointer; margin-left:10px" src="../imgs/hamburger-menu.svg" alt="">
+        <img data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
+            style="width: 40px; cursor:pointer;" src="../imgs/hamburger-menu.svg" alt="">
         <div class="border border-danger rounded-3 bg-white  text-danger  p-2 py-1"> Total Profiles :
             <span class="totalCount fw-bold fw-3 fs-5"></span>
         </div>
@@ -19,12 +19,12 @@
 <div style="max-width:70%" class="offcanvas offcanvas-start bg-warning-subtle" tabindex="-1" id="offcanvasExample"
     aria-labelledby="offcanvasExampleLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title text-danger" id="offcanvasExampleLabel">Admin</h5>
+        <h5 class="offcanvas-title text-danger" id="offcanvasExampleLabel">Admin </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-        <div>
-
+        <div class=" rounded-full overflow-hidden mb-3  text-center">
+            <img width="100px" style="border-radius:50%;" class="boorder border-circle shadow-sm" src="../imgs/logo.png" alt="">
         </div>
         <div class="d-flex flex-column gap-3">
             <a class="btn btn-danger" href="all-card.php">All Profiles</a>
