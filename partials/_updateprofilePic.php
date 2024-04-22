0c4198ff1b145014ae8e76fd6a255000b771af77
unknown <ankitbkana@outlook.com>
2024-04-22 20:06:59 +0530

    hello world

--- partials/_updateprofilePic.php
+++ partials/_updateprofilePic.php
@@ -1,20 +1,19 @@
 <?php
-include("_db.php");
+include ("_db.php");
 
 // Check if the form is submitted via AJAX
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["croppedImage"])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["croppedImage"]))
+{
     // Get the cropped image data
     $croppedImageData = $_POST["croppedImage"];
-
     // Decode the base64 encoded image data
     $croppedImage = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $croppedImageData));
 
     // Get the member ID
-
-    $memberId = $_POST['memberId'];
+    echo "your member id is " . $memberId = $_POST['memberId'];
 
     // Get the existing image file name from the database or any other source
-    $sql = "SELECT `pic` FROM `users` WHERE `id` = '$memberId'";
+    $sql = "SELECT `pic` FROM `users` WHERE `hash_id` = '$memberId'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
     $existingImage = $row['pic'];
@@ -29,15 +28,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["croppedImage"])) {
     file_put_contents($uploadDirectory . $newImageName, $croppedImage);
 
     // Update the database with the new image filename
-    $usql = "UPDATE `users` SET `pic`='$newImageName' WHERE `id` = '$memberId'";
+    $usql = "UPDATE `users` SET `pic`='$newImageName' WHERE `hash_id` = '$memberId'";
     $update = mysqli_query($conn, $usql);
 
     // Delete the existing image file if it's different from the default image
-    if ($existingImage !== 'defaultusers.png') {
+    if ($existingImage !== 'defaultusers.png')
+    {
         $delfile = $uploadDirectory . $existingImage;
-        if(unlink($delfile)){
-
-            echo json_encode(["success" => true, "message" => "new image updated","newImageName" => "$newImageName"]);
+        if (unlink($delfile))
+        {
+            if ($update)
+            {
+                echo json_encode(["success" => true, "message" => "new image updated", "newImageName" => "$newImageName"]);
+            }
         }
     }
 
