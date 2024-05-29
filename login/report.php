dcf44e90248d0ba006201d884c61bd88880be3e3
ankitjnvynr <ankitbkana@outlook.com>
2024-05-29 17:33:05 +0530

    dhfshdhhfdhsl

--- login/report.php
+++ login/report.php
@@ -1,17 +1,15 @@
 <?php
 session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true)
-{
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     header("location: index.php");
     exit;
 }
-include ("../partials/_db.php");
+include("../partials/_db.php");
 
 
 
 // filtering data
-if (isset($_POST['get-data']))
-{
+if (isset($_POST['get-data'])) {
     $sql = "SELECT * FROM `users` ORDER BY `id` DESC ";
 
     $filters = array();
@@ -21,35 +19,29 @@ if (isset($_POST['get-data']))
     $bytehsil = $_POST['bytehsil'];
     $bydikshit = $_POST['filterDikshit'];
 
-    if ($byCountry || $byState || $byCity || $bydikshit)
-    {
+    if ($byCountry || $byState || $byCity || $bydikshit) {
         $newStr = ' WHERE ';
     }
 
-    if ($byCountry)
-    {
+    if ($byCountry) {
         $filters = array();
         $byCountry = " country LIKE '" . $byCountry . "%'";
         array_push($filters, $byCountry);
     }
-    if ($byState)
-    {
+    if ($byState) {
         $byState = " state LIKE '" . $byState . "%'";
         array_push($filters, $byState);
     }
-    if ($filterdistrict)
-    {
+    if ($filterdistrict) {
         $filterdistrict = " district LIKE '" . $filterdistrict . "%'";
         array_push($filters, $filterdistrict);
     }
-    if ($bytehsil)
-    {
+    if ($bytehsil) {
         $bytehsil = " tehsil LIKE '" . $bytehsil . "%'";
         array_push($filters, $bytehsil);
     }
 
-    if ($bydikshit)
-    {
+    if ($bydikshit) {
         $bydikshit = " dikshit LIKE '" . $bydikshit . "%'";
         array_push($filters, $bydikshit);
     }
@@ -66,8 +58,7 @@ if (isset($_POST['get-data']))
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GIEO Gita : Members Reports</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body {
             background: #f7e092;
@@ -95,28 +86,23 @@ if (isset($_POST['get-data']))
     <!----------------------- filters input boxes--------------------- -->
     <div class="container">
         <form action="" method="POST" class="filterform d-flex flex-wrap gap-1">
-            <select required class="form-select form-select-sm" aria-label="Small select example" name="filterCountry"
-                id="countrySelect" onchange="SelectState(this)">
+            <select required class="form-select form-select-sm" aria-label="Small select example" name="filterCountry" id="countrySelect" onchange="SelectState(this)">
                 <option value="" selected>---Country---</option>
                 <?php
                 $optionSql = "SELECT DISTINCT `country` FROM `users` ORDER BY country ASC ";
                 $result = $conn->query($optionSql);
-                while ($row = mysqli_fetch_assoc($result))
-                {
+                while ($row = mysqli_fetch_assoc($result)) {
                     echo '<option value="' . $row['country'] . '">' . $row['country'] . '</option>';
                 }
                 ?>
             </select>
-            <select required name="filterState" class="form-select form-select-sm" aria-label="Small select example"
-                id="stateSelect" onchange="selectingdistrict(this)">
+            <select required name="filterState" class="form-select form-select-sm" aria-label="Small select example" id="stateSelect" onchange="selectingdistrict(this)">
                 <option value="" selected>---State---</option>
             </select>
-            <select name="filterdistrict" required name="filterCity" class="form-select form-select-sm"
-                aria-label="Small select example" id="districtSelect" onchange="selectingtehsil(this)">
+            <select name="filterdistrict" required name="filterCity" class="form-select form-select-sm" aria-label="Small select example" id="districtSelect" onchange="selectingtehsil(this)">
                 <option value="" selected>---District---</option>
             </select>
-            <select name="bytehsil" class="form-select form-select-sm" aria-label="Small select example"
-                id="tehsilSelect">
+            <select name="bytehsil" class="form-select form-select-sm" aria-label="Small select example" id="tehsilSelect">
                 <option value="" selected>---Tehsil---</option>
             </select>
             <select name="filterDikshit" class="form-select form-select-sm" aria-label="Small select example">
@@ -124,8 +110,7 @@ if (isset($_POST['get-data']))
                 <?php
                 $optionSql = "SELECT DISTINCT `dikshit` FROM `users` ";
                 $result = $conn->query($optionSql);
-                while ($row = mysqli_fetch_assoc($result))
-                {
+                while ($row = mysqli_fetch_assoc($result)) {
                     echo '<option value="' . $row['dikshit'] . '">' . $row['dikshit'] . '</option>';
                 }
                 ?>
@@ -161,15 +146,12 @@ if (isset($_POST['get-data']))
             <tbody>
                 Showing :-
                 <?php
-                if (isset($_POST['get-data']))
-                {
+                if (isset($_POST['get-data'])) {
                     $sr = 0;
                     $result = $conn->query($sql);
                     echo $totalresult = mysqli_num_rows($result);
-                    if ($totalresult > 0)
-                    {
-                        while ($row = mysqli_fetch_array($result))
-                        {
+                    if ($totalresult > 0) {
+                        while ($row = mysqli_fetch_array($result)) {
                             $sr++;
                             $user_id = $row['id'];
                             $country = $row['country'];
@@ -206,16 +188,14 @@ if (isset($_POST['get-data']))
                             </tr>
                             ';
                         }
-                    } else
-                    {
+                    } else {
                         echo '
                     <tr>
                         <td class="text-center" colspan="7"> No data found </td>
                     </tr>
                     ';
                     }
-                } else
-                {
+                } else {
                     echo '
                     <tr>
                         <td class="text-center" colspan="7"> Select filter to view data</td>
@@ -229,11 +209,9 @@ if (isset($_POST['get-data']))
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js"
-        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
     <script>
         $('.totalCount').load('_totalProfiles.php');
@@ -248,7 +226,7 @@ if (isset($_POST['get-data']))
                 data: {
                     country: e.value
                 },
-                success: function (response) {
+                success: function(response) {
                     let stateSelect = document.getElementById('stateSelect')
                     // console.log(response)
                     stateSelect.innerHTML = response;
@@ -263,7 +241,7 @@ if (isset($_POST['get-data']))
                 data: {
                     country: e.value
                 },
-                success: function (response) {
+                success: function(response) {
                     let stateSelect = document.getElementById('districtSelect')
                     // console.log(response)
                     stateSelect.innerHTML = response;
@@ -278,7 +256,7 @@ if (isset($_POST['get-data']))
                 data: {
                     country: e.value
                 },
-                success: function (response) {
+                success: function(response) {
                     let stateSelect = document.getElementById('tehsilSelect')
                     // console.log(response)
                     stateSelect.innerHTML = response;
@@ -288,9 +266,7 @@ if (isset($_POST['get-data']))
     </script>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
-        integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script>
         // Create a new Date object which represents the current date and time
@@ -330,15 +306,14 @@ if (isset($_POST['get-data']))
 
 
     <script src="https://unpkg.com/jspdf@latest/dist/jspdf.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js"
-        integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/"
-        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.6/jspdf.plugin.autotable.min.js"></script>
 
     <script>
         var specialElementHandlers = {
             // element with id of "bypass" - jQuery style selector
-            '.no-export': function (element, renderer) {
+            '.no-export': function(element, renderer) {
                 // true = "handled elsewhere, bypass text extraction"
                 return true;
             }
@@ -354,7 +329,7 @@ if (isset($_POST['get-data']))
             pageHeight = doc.internal.pageSize.height;
             specialElementHandlers = {
                 // element with id of "bypass" - jQuery style selector  
-                '#bypassme': function (element, renderer) {
+                '#bypassme': function(element, renderer) {
                     // true = "handled elsewhere, bypass text extraction"  
                     return true
                 }
@@ -375,9 +350,8 @@ if (isset($_POST['get-data']))
                 theme: 'grid',
 
             })
-            doc.save(`GIEO GITA ${dateString}.pdf`);
+            doc.save(`GIEO GITA report <?php echo $tehsil ?> ${dateString}.pdf`);
         }
-
     </script>
 
 </body>
