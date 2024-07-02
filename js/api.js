5c75ca140d097a632377195e44b18920cdd6ebff
ankitjnvynr <ankitbkana@outlook.com>
2024-07-02 16:34:22 +0530

    api via js

--- js/api.js
+++ js/api.js
@@ -48,6 +48,54 @@ setInterval(() => {
 
 
 
+
+// function to send message with api
+
+
+async function sendWaMessages( phone, message, filePath, caption) {
+    
+
+    // API URL
+    const baseUrl = 'https://app.jflindia.co.in/api/v1/message/create';
+
+    // Data to be sent as query parameters
+    const data = {
+        username: 'gieo2024',
+        password: 'Gieo@2024',
+        receiverMobileNo: phone,
+        message: message,
+        filePathUrl: filePath,
+        caption: caption
+    };
+
+    // Build the full URL with query parameters
+    const url = `${baseUrl}?${new URLSearchParams(data).toString()}`;
+
+    try {
+        // Send the request
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+
+        // Check if the response is successful
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        const responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        return { error: error.message };
+    }
+}
+
+
+
+
+// message sending script 
+
+
 $(document).ready(()=>{
     const form = document.getElementById('getDataForm');
     form.addEventListener('submit', function(event) {
@@ -66,8 +114,11 @@ $(document).ready(()=>{
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            // Handle the JSON data
+            console.log(typeof data);
+            // console.log(data)
+            console.log(data.users)
+            console.log(data.users[0].phone)
+            // sendWaMessages(data.phone)
         })
         .catch(error => console.error('Error:', error));
     });
--- login/api2/getData.php
+++ login/api2/getData.php
@@ -1,132 +1,128 @@
 <?php
-session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-    header("location: index.php");
-    exit;
-}
+
 include("../../partials/_db.php");
 
 $currentURL = "https://$_SERVER[HTTP_HOST]/";
 $response = [];
 
-if (isset($_POST['get-data'])) {
-    $sql = "SELECT * FROM `users` ORDER BY `id` DESC ";
-    $filters = array();
-    $byCountry = $_POST['filterCountry'];
-    $byState = $_POST['filterState'];
-    $filterdistrict = $_POST['filterdistrict'];
-    $bytehsil = $_POST['bytehsil'];
-    $selectedMediaIds = isset($_POST['selectedMedia']) ? json_decode($_POST['selectedMedia'], true) : [];
+
+$sql = "SELECT * FROM `users` ORDER BY `id` DESC ";
+$filters = array();
+$byCountry = $_POST['filterCountry'];
+$byState = $_POST['filterState'];
+$filterdistrict = $_POST['filterdistrict'];
+$bytehsil = $_POST['bytehsil'];
+$selectedMediaIds = isset($_POST['selectedMedia']) ? json_decode($_POST['selectedMedia'], true) : [];
 
 
-    if ($byCountry || $byState || $filterdistrict || $bytehsil) {
-        $newStr = ' WHERE ';
+if ($byCountry || $byState || $filterdistrict || $bytehsil) {
+    $newStr = ' WHERE ';
+}
+
+if ($byCountry) {
+    $byCountry = " country LIKE '" . $byCountry . "%'";
+    array_push($filters, $byCountry);
+}
+if ($byState) {
+    $byState = " state LIKE '" . $byState . "%'";
+    array_push($filters, $byState);
+}
+if ($filterdistrict) {
+    $filterdistrict = " district LIKE '" . $filterdistrict . "%'";
+    array_push($filters, $filterdistrict);
+}
+if ($bytehsil) {
+    $bytehsil = " tehsil LIKE '" . $bytehsil . "%'";
+    array_push($filters, $bytehsil);
+}
+
+$newStr = $newStr . implode(" AND ", $filters);
+$sql = "SELECT * FROM `users`  " . $newStr . "  ORDER BY name ASC;";
+
+// Fetch selected media paths and captions
+$mediaPaths = [];
+$mediaCaptions = [];
+if (!empty($selectedMediaIds)) {
+    $ids = implode(",", $selectedMediaIds);
+    $mediaSql = "SELECT image_path, image_caption FROM api_content WHERE id IN ($ids)";
+    $mediaResult = $conn->query($mediaSql);
+
+    while ($mediaRow = $mediaResult->fetch_assoc()) {
+        $mediaPaths[] = $mediaRow['image_path'];
+        $mediaCaptions[] = $mediaRow['image_caption'];
     }
+}
 
-    if ($byCountry) {
-        $byCountry = " country LIKE '" . $byCountry . "%'";
-        array_push($filters, $byCountry);
+function convert_to_str($arr, $currentURL)
+{
+    $prefix = $currentURL . 'imgs/api_content/';
+    $prefixedArray = array_map(function ($item) use ($prefix) {
+        return $prefix . $item;
+    }, $arr);
+    return implode(',', $prefixedArray);
+}
+$allMediaStr = convert_to_str($mediaPaths, $currentURL);
+$allCaptionStr = implode(',', $mediaCaptions);
+
+$country_code = array(
+    "Australia" => "61",
+    "Canada" => "1",
+    "United Kingdom" => "44",
+    "India" => "91",
+    "Japan" => "81",
+    "New Zealand" => "64",
+    "United Arab Emirates" => "971",
+    "United States" => "1",
+    "USA" => "1",
+    "UK" => "+44",
+    "England" => "+44",
+);
+
+
+$messageSelect = $_POST['messageSelect'];
+$msgsql = "SELECT * FROM `messages` WHERE `title` = '$messageSelect'";
+$msgresult = mysqli_query($conn, $msgsql);
+$msgrow = mysqli_fetch_assoc($msgresult);
+$message = $msgrow['msg'];
+
+$users = [];
+$result = $conn->query($sql);
+$totalresult = mysqli_num_rows($result);
+if ($totalresult > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        $code = $country_code[$row['country']];
+        $user = [
+            'id' => $row['id'],
+            'country' => $row['country'],
+            'name' => $row['name'],
+            'phone' => $code.$row['phone'],
+            'designation' => $row['designation'],
+            'email' => $row['email'],
+            'dikshit' => $row['dikshit'],
+            'marital_status' => $row['marital_status'],
+            'state' => $row['state'],
+            'district' => $row['district'],
+            'tehsil' => $row['tehsil'],
+            'address' => $row['address'],
+            'wing' => $row['interest'],
+            'occupation' => $row['occupation'],
+            'education' => $row['education'],
+            'dob' => $row['dob'],
+            'aniver_date' => $row['aniver_date'],
+            'joinOn' => $row['dt'],
+            'pic' => $row['pic'],
+            'message' => 'गीता प्रिय ' . $row['name'] . ' जी , 🌹 * जय श्री कृष्ण * 🌹 ' . urldecode(html_entity_decode($message)) . ' To view profile Click here- ' . $currentURL . 'member.php?member=' . md5($row['phone'])
+        ];
+        $users[] = $user;
     }
-    if ($byState) {
-        $byState = " state LIKE '" . $byState . "%'";
-        array_push($filters, $byState);
-    }
-    if ($filterdistrict) {
-        $filterdistrict = " district LIKE '" . $filterdistrict . "%'";
-        array_push($filters, $filterdistrict);
-    }
-    if ($bytehsil) {
-        $bytehsil = " tehsil LIKE '" . $bytehsil . "%'";
-        array_push($filters, $bytehsil);
-    }
+    $response['users'] = $users;
+} else {
+    $response['users'] = [];
+}
 
-    $newStr = $newStr . implode(" AND ", $filters);
-    $sql = "SELECT * FROM `users`  " . $newStr . "  ORDER BY name ASC;";
+$response['mediaPaths'] = $allMediaStr;
+$response['mediaCaptions'] = $allCaptionStr;
 
-    // Fetch selected media paths and captions
-    $mediaPaths = [];
-    $mediaCaptions = [];
-    if (!empty($selectedMediaIds)) {
-        $ids = implode(",", $selectedMediaIds);
-        $mediaSql = "SELECT image_path, image_caption FROM api_content WHERE id IN ($ids)";
-        $mediaResult = $conn->query($mediaSql);
-
-        while ($mediaRow = $mediaResult->fetch_assoc()) {
-            $mediaPaths[] = $mediaRow['image_path'];
-            $mediaCaptions[] = $mediaRow['image_caption'];
-        }
-    }
-
-    function convert_to_str($arr, $currentURL)
-    {
-        $prefix = $currentURL . 'imgs/api_content/';
-        $prefixedArray = array_map(function ($item) use ($prefix) {
-            return $prefix . $item;
-        }, $arr);
-        return implode(',', $prefixedArray);
-    }
-    $allMediaStr = convert_to_str($mediaPaths, $currentURL);
-    $allCaptionStr = implode(',', $mediaCaptions);
-
-    $country_code = array(
-        "Australia" => "61",
-        "Canada" => "1",
-        "United Kingdom" => "44",
-        "India" => "91",
-        "Japan" => "81",
-        "New Zealand" => "64",
-        "United Arab Emirates" => "971",
-        "United States" => "1",
-        "USA" => "1",
-        "UK" => "+44",
-        "England" => "+44",
-    );
-
-    
-        $messageSelect = $_POST['messageSelect'];
-        $msgsql = "SELECT * FROM `messages` WHERE `title` = '$messageSelect'";
-        $msgresult = mysqli_query($conn, $msgsql);
-        $msgrow = mysqli_fetch_assoc($msgresult);
-        $message = $msgrow['msg'];
-
-        $users = [];
-        $result = $conn->query($sql);
-        $totalresult = mysqli_num_rows($result);
-        if ($totalresult > 0) {
-            while ($row = mysqli_fetch_array($result)) {
-                $code = $country_code[$row['country']];
-                $user = [
-                    'id' => $row['id'],
-                    'country' => $row['country'],
-                    'name' => $row['name'],
-                    'phone' => $row['phone'],
-                    'designation' => $row['designation'],
-                    'email' => $row['email'],
-                    'dikshit' => $row['dikshit'],
-                    'marital_status' => $row['marital_status'],
-                    'state' => $row['state'],
-                    'district' => $row['district'],
-                    'tehsil' => $row['tehsil'],
-                    'address' => $row['address'],
-                    'wing' => $row['interest'],
-                    'occupation' => $row['occupation'],
-                    'education' => $row['education'],
-                    'dob' => $row['dob'],
-                    'aniver_date' => $row['aniver_date'],
-                    'joinOn' => $row['dt'],
-                    'pic' => $row['pic'],
-                    'message' => 'गीता प्रिय ' . $row['name'] . ' जी , 🌹 * जय श्री कृष्ण * 🌹 ' . urldecode(html_entity_decode($message)) . ' To view profile Click here- ' . $currentURL . 'member.php?member=' . md5($row['phone'])
-                ];
-                $users[] = $user;
-            }
-            $response['users'] = $users;
-        } else {
-            $response['users'] = [];
-        }
-
-        $response['mediaPaths'] = $allMediaStr;
-        $response['mediaCaptions'] = $allCaptionStr;
-    }
 
 
 header('Content-Type: application/json');
